61092531dee7eb9ccc3a033246d7b3476e9bf9e0
Nick Frasser <nfrasser@gmail.com>
2023-10-21 18:25:02 -0400

    Update type defintions

--- oxipng.pyi
+++ oxipng.pyi
@@ -3,7 +3,7 @@ Python wrapper for multithreaded .png image file optimizer oxipng
 (https://github.com/shssoichiro/oxipng - written in Rust). Use this module to
 reduce the file size of your PNG images.
 """
-from typing import Collection, Optional, Union
+from typing import Collection, List, Optional, Union
 from enum import Enum
 from os import PathLike
 
@@ -38,7 +38,7 @@ class StripChunks:
         ...
 
     @staticmethod
-    def strip(val: Collection[str]) -> "StripChunks":
+    def strip(val: Collection[bytes]) -> "StripChunks":
         ...
 
     @staticmethod
@@ -46,7 +46,7 @@ class StripChunks:
         ...
 
     @staticmethod
-    def keep(val: Collection[str]) -> "StripChunks":
+    def keep(val: Collection[bytes]) -> "StripChunks":
         ...
 
     @staticmethod
@@ -79,6 +79,68 @@ class Libdeflater:
     """
 
     def __init__(self) -> None:
+        ...
+
+
+class ColorType:
+    @staticmethod
+    def grayscale(transparent_shade: Optional[int] = None) -> "ColorType":
+        ...
+
+    @staticmethod
+    def rgb(transparent_color: Optional[Collection[int]] = None) -> "ColorType":
+        ...
+
+    @staticmethod
+    def indexed(palette: List[Collection[int]]) -> "ColorType":
+        ...
+
+    @staticmethod
+    def grayscale_alpha() -> "ColorType":
+        ...
+
+    @staticmethod
+    def rgba() -> "ColorType":
+        ...
+
+
+class RawImage:
+    def __init__(
+        self,
+        data: Union[bytes, bytearray],
+        width: int,
+        height: int,
+        *,
+        color_type: ColorType = ...,
+        bit_depth: int = 8,
+    ) -> None:
+        ...
+
+    def add_png_chunk(self, name: bytes, data: Union[bytes, bytearray]) -> None:
+        ...
+
+    def add_icc_profile(self, data: bytes) -> None:
+        ...
+
+    def create_optimized_png(
+        self,
+        level: int = 2,
+        fix_errors: bool = False,
+        force: bool = False,
+        filter: Collection[RowFilter] = {RowFilter.NoOp},
+        interlace: Optional[Interlacing] = None,
+        optimize_alpha: bool = False,
+        bit_depth_reduction: bool = True,
+        color_type_reduction: bool = True,
+        palette_reduction: bool = True,
+        grayscale_reduction: bool = True,
+        idat_recoding: bool = True,
+        scale_16: bool = False,
+        strip: StripChunks = StripChunks.none(),
+        deflate: Deflaters = Deflaters.libdeflater(11),
+        use_heuristics: bool = False,
+        timeout: Optional[int] = None,
+    ):
         ...
 
 
